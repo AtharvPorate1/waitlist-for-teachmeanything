6e69bebdf3b5416275c899b824be17894b07845a
[AtharvPorate1] <www.atharvporate254@gmail.com>
2023-05-23 22:42:56 +0530

    trying replit for deployment

--- public/index.js
+++ public/index.js
@@ -1,26 +1,31 @@
 // Handle form submission
-document.getElementById('emailForm').addEventListener('submit', (e) => {
+document.getElementById('emailForm').addEventListener('submit', async (e) => {
   e.preventDefault();
 
   const emailInput = document.getElementById('search-bar');
   const email = emailInput.value.trim();
 
   if (email) {
-    // Send a POST request to the server with the email data
-    fetch('/waitlist', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
+    try {
+      // Send a POST request to the server with the email data
+      const response = await fetch('/waitlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
         emailInput.value = '';
         emailInput.placeholder = data.message;
-      })
-      .catch((error) => {
-        console.error('Error submitting email:', error);
-      });
+      } else {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.message || 'Request failed');
+      }
+    } catch (error) {
+      console.error('Error submitting email:', error);
+    }
   }
 });
